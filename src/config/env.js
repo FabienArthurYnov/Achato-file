ffd785ebeae661305719a59b3c361ea6813c519a
ThibautFgi <thibaut.fgi@hotmail.com>
2026-01-20 12:04:05 +0100

    deux microservices ez

--- src/config/env.js
+++ src/config/env.js
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: '.env.local' });
 
 export const env = {
-    port: process.env.PORT || 4003,
-    nodeEnv: process.env.NODE_ENV || 'development',
+    port: process.env.PORT,
+    nodeEnv: process.env.NODE_ENV,
+    authServiceToken: process.env.AUTH_SERVICE_TOKEN,
     db: {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT || '3306',
+        port: process.env.DB_PORT,
         name: process.env.DB_NAME,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN,
     },
 };
